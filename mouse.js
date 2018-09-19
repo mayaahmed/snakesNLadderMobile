c3e21cf26d47cb15da574b26109828f4ed32a89d
Maya Mohsin Ahmed <maya.ahmed@gmail.com>
2018-09-19 18:46:01 +0530

    Add files via upload

--- mouse.js
+++ mouse.js
@@ -1,8 +1,71 @@
+function init() {
+
+
 canvas.addEventListener('mousedown', imageMouseDown, false);
     canvas.addEventListener('mousemove', imageMouseMove, false);
     canvas.addEventListener('mouseup',imageMouseUp, false);
     canvas.addEventListener('mouseout',imageMouseOut, false);
 
+drawKandis();
+
+
+// Set up touch events for mobile, etc
+canvas.addEventListener("touchstart", function (e) {
+    mousePos = getTouchPos(canvas, e);
+    var touch = e.touches[0];
+    var mouseEvent = new MouseEvent("mousedown", {
+	clientX: touch.clientX,
+	clientY: touch.clientY
+    });
+    canvas.dispatchEvent(mouseEvent);
+}, false);
+canvas.addEventListener("touchend", function (e) {
+    var mouseEvent = new MouseEvent("mouseup", {});
+    canvas.dispatchEvent(mouseEvent);
+}, false);
+canvas.addEventListener("touchmove", function (e) {
+    var touch = e.touches[0];
+    var mouseEvent = new MouseEvent("mousemove", {
+	clientX: touch.clientX,
+	clientY: touch.clientY
+    });
+    canvas.dispatchEvent(mouseEvent);
+}, false);
+
+}
+
+window.addEventListener('load', init, false);
 
 
 
+
+
+
+
+// Get the position of a touch relative to the canvas
+function getTouchPos(canvasDom, touchEvent) {
+    var rect = canvasDom.getBoundingClientRect();
+    return {
+	x: touchEvent.touches[0].clientX - rect.left,
+	y: touchEvent.touches[0].clientY - rect.top
+    };
+}
+
+
+// Prevent scrolling when touching the canvas
+document.body.addEventListener("touchstart", function (e) {
+    if (e.target == canvas) {
+	e.preventDefault();
+    }
+}, false);
+document.body.addEventListener("touchend", function (e) {
+    if (e.target == canvas) {
+	e.preventDefault();
+    }
+}, false);
+
+document.body.addEventListener("touchmove", function (e) {
+    if (e.target == canvas) {
+	e.preventDefault();
+    }
+}, false);
